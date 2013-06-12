d6abfc6f558f2f28fa3de57350ba2621008508b4
Evan McClain <aeroevan@gmail.com>
2013-06-12 16:18:08 -0400

    python3 fixes for parse_metar.py
    Signed-off-by: Evan McClain <aeroevan@gmail.com>

--- parse_metar.py
+++ parse_metar.py
@@ -1,17 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # simple command-line driver for metar parser
 #
-import sys, os
-from metar import Metar
+import sys
+import os
+from metar import metar
 import string
 import getopt
-import profile, pstats
+import profile
+import pstats
+
 
 def usage():
     program = os.path.basename(sys.argv[0])
     print(("Usage: %s [-s] [<file>]" % program))
-    options =  """Options:
+    options = """Options:
         <file> ... a file containing METAR reports to parse
         -q ....... run "quietly" - just report parsing error.
         -s ....... run silently. (no output)
@@ -40,31 +43,33 @@ try:
             report = False
         elif opt[0] == '-d':
             debug = True
-            Metar.debug = True
+            metar.debug = True
         elif opt[0] == '-p':
             prof = True
 except:
-  usage()
+    usage()
+
 
 def process_line(line):
     """Decode a single input line."""
     line = line.strip()
-    if len(line) and line[0] in string.uppercase:
+    if len(line) and line[0] in string.ascii_uppercase:
         try:
-            obs = Metar.Metar(line)
+            obs = metar.Metar(line)
             if report:
                 print("--------------------")
                 print((obs.string()))
-        except Metar.ParserError as err:
+        except metar.ParserError as err:
             if not silent:
                 print("--------------------")
                 print(("METAR code: %s" % line))
-                print((string.join(err.args,", ")))
+                print((string.join(err.args, ", ")))
+
 
 def process_files(files):
     """Decode METAR lines from the given files."""
     for file in files:
-        fh = open(file,"r")
+        fh = open(file, "r")
         for line in fh.readlines():
             process_line(line)
 
@@ -74,15 +79,16 @@ if files:
     else:
         process_files(files)
 else:
-    # read lines from stdin 
+    # read lines from stdin
     while True:
-      try:
-          line = sys.stdin.readline()
-          if line == "":
-             break
-          process_line(line)
-      except KeyboardInterrupt:
-          break
+        try:
+            line = sys.stdin.readline()
+            if line == "":
+                break
+            process_line(line)
+        except KeyboardInterrupt:
+            break
+
 
 if prof:
     ps = pstats.load('metar.prof')
